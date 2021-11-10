c9dfed3258f8aa146cde95e82d26eb16bf5b6121
Ezaz Ahmed <ezaznahmed@gmail.com>
2021-11-10 11:05:34 +0600

    Hotel data in session & user data in Local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,29 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
+import sessionStorage from 'redux-persist/lib/storage/session';
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import hotelReducer from './feature/hotel/hotelSlice';
 import userReducer from './feature/user/userSlices';
 
-const reducers = combineReducers({
-  hotel: hotelReducer,
-  user: userReducer,
-});
-
-const persistConfig = {
+const rootPersistConfig = {
   key: 'root',
   storage,
+  whitelist: ['user'],
 };
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+const hotelPersistConfig = {
+  key: 'hotel',
+  storage: sessionStorage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  hotel: persistReducer(hotelPersistConfig, hotelReducer),
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
