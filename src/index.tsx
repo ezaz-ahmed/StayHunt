56724c871c7660d6cd39ffb5120f7496eac2a85e
Ezaz <ezaznahmed@gmail.com>
2022-01-01 17:07:48 +0600

    Before restart

--- src/index.tsx
+++ src/index.tsx
@@ -10,22 +10,18 @@ import './index.css';
 import './fonts/line-awesome-1.3.0/css/line-awesome.css';
 //
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import { QueryClient, QueryClientProvider } from 'react-query';
-//
+import { persistStore } from 'redux-persist';//
 import App from './App';
 //
 
 let persistor = persistStore(store);
-const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </QueryClientProvider>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  ,
   document.getElementById('root')
 );
